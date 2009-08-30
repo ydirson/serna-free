b1fe35ba5a59568e51f5a17ffff9d00be8e2e355
Integrator of the Day <abs@syntext.com>
2009-08-30 10:47:07 +0000

    fixed doc manifest
    git-svn-id: svn://oss.syntext.com/oss/serna-free/trunk@119 76c9efe7-43af-4aca-a270-d8da3bf5252a

--- serna/dist/doc-dita/genmft.sh
+++ serna/dist/doc-dita/genmft.sh
@@ -3,6 +3,6 @@
 ../../../tools/manifest/lst2mft.py \
         -d '${inst_prefix}/${serna}/doc' \
         -s '${top_builddir}/${apps_serna}/dist/doc-dita' \
-        -x serna-help.html \
+        -x serna-help.html -x files.lst -x assistant.png -x '.*\.qh.?p' \
         -o MANIFEST.doc-dita.built \
         doc-dita
