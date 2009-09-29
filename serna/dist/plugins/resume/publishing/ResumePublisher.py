0ab39c283701d8a2efcf13c8e9ceff12513a3d1d
Michael Korolev <korolevma@syntext.com>
2009-09-29 14:53:33 +0000

    added publishing
    git-svn-id: svn://oss.syntext.com/oss/serna-free/trunk@177 76c9efe7-43af-4aca-a270-d8da3bf5252a

---
+++ serna/dist/plugins/resume/publishing/ResumePublisher.py
@@ -0,0 +1,69 @@
+## 
+## Copyright(c) 2009 Syntext, Inc. All Rights Reserved.
+## Contact: info@syntext.com, http://www.syntext.com
+## 
+## This file is part of Syntext Serna XML Editor.
+## 
+## COMMERCIAL USAGE
+## Licensees holding valid Syntext Serna commercial licenses may use this file
+## in accordance with the Syntext Serna Commercial License Agreement provided
+## with the software, or, alternatively, in accorance with the terms contained
+## in a written agreement between you and Syntext, Inc.
+## 
+## GNU GENERAL PUBLIC LICENSE USAGE
+## Alternatively, this file may be used under the terms of the GNU General 
+## Public License versions 2.0 or 3.0 as published by the Free Software 
+## Foundation and appearing in the file LICENSE.GPL included in the packaging 
+## of this file. In addition, as a special exception, Syntext, Inc. gives you
+## certain additional rights, which are described in the Syntext, Inc. GPL 
+## Exception for Syntext Serna Free Edition, included in the file 
+## GPL_EXCEPTION.txt in this package.
+## 
+## You should have received a copy of appropriate licenses along with this 
+## package. If not, see <http://www.syntext.com/legal/>. If you are unsure
+## which license is appropriate for your use, please contact the sales 
+## department at sales@syntext.com.
+## 
+## This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+## WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+## 
+from SimplePublisher import *
+from publishing import PublishingPlugin
+
+__all__ = ['register_creators']
+
+class ResumeHtmlPublisher(SimpleHtmlPublisher):
+    def __init__(self, **kwargs):
+        SimpleHtmlPublisher.__init__(self, **kwargs)
+        self.name = 'Resume to HTML publisher'
+        self.doctype = 'Resume'
+
+class ResumeFoPublisher(SimpleFoPublisher):
+    def __init__(self, **kwargs):
+        SimpleFoPublisher.__init__(self, **kwargs)
+        self.name = 'Resume to FO(PDF) publisher'
+        self.doctype = 'Resume'
+
+class TeiPublisherCreator(SimplePublisherCreator):
+    def __init__(self, **kwargs):
+        SimplePublisherCreator.__init__(self, 'Resume publisher creator',
+                                        FoPublisher=ResumeFoPublisher,
+                                        HtmlPublisher=ResumeHtmlPublisher,
+                                        **kwargs)
+
+        xslDir = os.path.join(self.pluginDir, 'xsl')
+
+        self.xslFoScript = os.path.join(xslDir, 'pdf/format/fo.xsl')
+        self.singleXsl = os.path.join(xslDir, 'pdf/format/html.xsl')
+
+    def _matchDoctype(self, category, name, tags):
+        if category.startswith('XML Resume'):
+            return [ tag for tag in tags if not 'Multiple' in tag ]
+        return []
+
+def register_creators(pluginDir):
+    registry = PublishingPlugin.PublishingPlugin.getPublishersRegistry()
+    if registry:
+        pc = TeiPublisherCreator(pluginDir=pluginDir)
+        registry.registerPublisherCreator(pc)
+
