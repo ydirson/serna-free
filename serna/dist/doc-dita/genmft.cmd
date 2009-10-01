c4dbe665ea1edf2d8d16202295fe9d049026590d
Integrator of the Day <abs@syntext.com>
2009-10-01 14:08:06 +0000

    fixed manifest for documentation and missing dita files
    git-svn-id: svn://oss.syntext.com/oss/serna-free/trunk@181 76c9efe7-43af-4aca-a270-d8da3bf5252a

--- serna/dist/doc-dita/genmft.cmd
+++ serna/dist/doc-dita/genmft.cmd
@@ -1,4 +1,4 @@
 @echo off
 set ARGS=-d '${inst_prefix}/${serna}/doc' -s '${top_builddir}/${apps_serna}/dist/doc-dita' 
-set ARGS=%ARGS% -x serna-help.html -o MANIFEST.doc-dita.built doc-dita
-python ..\..\..\tools\manifest\lst2mft.py %ARGS%
+set ARGS=%ARGS% -x serna-help.html -x files.lst -x assistant.png -x .*\.qh.?p -o MANIFEST.doc-dita.built doc-dita
+python %~dp0..\..\..\tools\manifest\lst2mft.py %ARGS%
--- serna/dist/doc-dita/genmft.sh
+++ serna/dist/doc-dita/genmft.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-../../../tools/manifest/lst2mft.py \
+`dirname $0`/../../../tools/manifest/lst2mft.py \
         -d '${inst_prefix}/${serna}/doc' \
         -s '${top_builddir}/${apps_serna}/dist/doc-dita' \
         -x serna-help.html -x files.lst -x assistant.png -x '.*\.qh.?p' \
