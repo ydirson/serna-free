e142b3da051b5b904d13c562c6f5026cafc86aa5
Michael Korolev <korolevma@syntext.com>
2009-10-02 11:15:52 +0000

    corrected
    git-svn-id: svn://oss.syntext.com/oss/serna-free/trunk@185 76c9efe7-43af-4aca-a270-d8da3bf5252a

--- serna/dist/plugins/publishing/Publisher.py
+++ serna/dist/plugins/publishing/Publisher.py
@@ -27,7 +27,7 @@
 ## This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
 ## WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
 ## 
-from utils import Value, Creator
+from utils import Value, Creator, PublishException
 from codecs import getencoder
 from PyQt4 import QtCore
 
