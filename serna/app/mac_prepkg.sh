374af36a218ec6303b35365310443628a18ee704
Paul Antonov <apg@syntext.com>
2010-07-16 20:19:25 +0000

    macos install fixes

--- serna/app/mac_prepkg.sh
+++ serna/app/mac_prepkg.sh
@@ -7,3 +7,6 @@ CONTENTS=${SERNA_PKGROOT}/Contents
 python ${srcdir}/fix_install_names.py ${CONTENTS} serna.bin
 
 strip -Sx `find ${CONTENTS} -type f -name "*.bin" -o -name "*.dylib" -o -name "*.so"`
+
+RESDIR=${SERNA_PKGROOT}/../../Resources
+chmod a+x $RESDIR/postinstall $RESDIR/postupgrade
