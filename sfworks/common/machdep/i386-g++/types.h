aadf78df2ed5d357b08c1aee10f8a3ff627a5202
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2011-04-10 19:07:11 +0200

    Improve typdefs for integer on non x86 platform

--- sfworks/common/machdep/i386-g++/types.h
+++ sfworks/common/machdep/i386-g++/types.h
@@ -33,16 +33,16 @@
 #define MACHDEP_I386_GNUC_TYPES_H_
 
 #include "common/common_defs.h"
+#include <stdint.h>
 
-
-typedef char           int8;
-typedef unsigned char  uint8;
-typedef short          int16;
-typedef unsigned short uint16;
-typedef long            int32;
-typedef unsigned long   uint32;
-typedef long long      int64;
-typedef unsigned long long uint64;
+typedef int8_t int8;
+typedef uint8_t uint8;
+typedef int16_t int16;
+typedef uint16_t uint16;
+typedef int32_t int32;
+typedef uint32_t uint32;
+typedef int64_t int64;
+typedef uint64_t uint64;
 
 #define UINT64_VALUE(x) x##ULL
 #define INT64_VALUE(x)  x##LL
