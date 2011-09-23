acdafa5bb687839709d619f8ece6fb85ddd86a9c
Paul Antonov <apg@syntext.com>
2011-09-23 17:53:54 +0400

    fixed hunspell mac install

--- 3rd/hunspell/install.sh
+++ 3rd/hunspell/install.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+
+SFX=so
 . ${THIRD_DIR}/functions
-install_shlib ${INST_SRCDIR} ${THIRD_DIR}/lib libhunspell
+
+if [ x${PLATFORM} = xdarwin ]; then
+    SFX=dylib
+fi
+symlink ../${INST_RELDIR}/libhunspell.${SFX} ${THIRD_DIR}/lib
 ln -sf ${INST_SRCDIR}/dict ${THIRD_DIR}/hunspell/dict
